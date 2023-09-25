b4a831a06f36079c6913dedb24106fb61049d006
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-09-25 16:35:56 +0300

    d

---
+++ 894div3/d.py
@@ -0,0 +1,31 @@
+def cn2(n):
+    return (n * (n - 1)) // 2
+
+def solution(test):
+    left = 0;
+    right = MAXNUMBER - 1;
+    f = False;
+    while (left < right):
+        mid = (left + right) // 2;
+        tcn = cn2(mid);
+        if (tcn < test):
+            left = mid + 1;
+        else:
+            right = mid;
+
+    if (cn2(left) == test):
+        f = True;
+        return int(left);
+    if (not f):
+        while (cn2(left) > test):
+            left -= 1;
+    left += test - cn2(left);
+    return int(left);
+
+
+MAXNUMBER = 2648956421 + 1;
+
+n = int(input())
+
+for i in range(n):
+    print(solution(int(input())))
