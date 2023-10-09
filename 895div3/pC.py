ad73f38a5cbf3b646920af863e18094b2807c1e7
Linux Torbolts <114951816+MakimaGorky@users.noreply.github.com>
2023-10-09 14:34:40 +0300

    PYC

--- 895div3/pC.py
+++ 895div3/pC.py
@@ -1,7 +1,53 @@
+import math
 
-isPrime = [True] * (n + 1)
-for i in range(2, n + 1):
-    if not isPrime[i]:
-        continue
-    for j in range(i + i, n + 1, i):
-        if
+def gcd(a, b):
+    while ((a != 0) and (b != 0)):
+        if (a > b):
+            a = a % b
+        else:
+            b = b % a
+    return a + b
+
+def eratosphenes(n):
+    primes = [True for i in range(n + 1)]
+
+    i = 2;
+    while (i <= n):
+        if primes[i]:
+            j = i + i
+            while j <= n:
+                primes[j] = False
+                j += i
+        i += 1
+
+    return primes
+
+def getSomeDiv(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if not(n % i):
+            return i
+
+primes = eratosphenes(int(1e7))
+
+def sol_C(l, r):
+    if r < 4:
+        print(-1)
+        return
+
+    for i in range(l, r + 1):
+        if not primes[i]: # todo bin search
+            div = getSomeDiv(i)
+            print(div, i - div)
+            return
+
+    print(-1)
+
+
+MAXNUMBER = 1e7;
+
+# for i in eratosphenes(int(MAXNUMBER)):
+#     print(i, end=', ')
+
+for t in range(int(input())):
+    l, r = map(int, input().split())
+    sol_C(l, r)
