1bc34252b0314ecb3959e7786c5ed2b1df5f74a3
VIPigeon <slv.bta@gmail.com>
2023-10-16 13:49:48 +0300

    D solve

--- 883div3/pD.py
+++ 883div3/pD.py
@@ -2,5 +2,20 @@
 def getS(d, h, y):
     # y -- координата среза относительно верхней точки
     # d, h -- параметры исходного треугольника
-    p = h / d
-    
+    p = y / h
+    return y * p * d / 2
+
+
+def solution():
+    n, d, h = map(int, input().split())
+    coords = list(map(int, input().split()))
+    res = d * h / 2
+    for prev, y in zip(coords, coords[1:]):
+        res += d * h / 2
+        if prev + h > y:
+            res -= getS(d, h, prev + h - y)
+    print(res)
+
+
+for _ in range(int(input())):
+    solution()
