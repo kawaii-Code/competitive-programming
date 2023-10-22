b27ee43c18d4760503e7e7c1384eea127def0b7a
kawaii-Code <nia.personal.0@gmail.com>
2023-10-22 16:15:30 +0300

    Shorter template, split in separate files

--- template/template.cpp
+++ template/template.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
+#include <string>
+
+#define debug(x) std::cout << #x << " = " << x << "\n"
 
 typedef unsigned long long ull;
 typedef long long ll;
 
 using namespace std;
-
-#define debug(x) std::cout << #x << " = " << x << "\n"
-#define debug2(x, y) std:: cout << #x << " = " << x << " " << #y << " = " << y << "\n";
-#define debug3(x, y, z) std:: cout << #x << " = " << x << " " << #y << " = " << y << "\n" << " " << #z << " = " << z << "\n";
 
 void solve() {
 }
